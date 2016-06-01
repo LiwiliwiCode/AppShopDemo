6239fa7b755acd6a81d7e915c6a3b5edca67d1eb
bbyklf <554140410@qq.com>
2016-06-01 17:16:31 +0800

    添加

---
+++ app/src/main/java/com/shop/fragment/BaseFragment.java
@@ -0,0 +1,101 @@
+package com.shop.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import shop.com.appshopdemo.R;
+
+public class BaseFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Fragment 是否可见标识
+     */
+    private boolean isVisible ;
+
+    private OnFragmentInteractionListener mListener;
+
+    public BaseFragment() {
+    }
+
+    public static BaseFragment newInstance(String param1, String param2) {
+        BaseFragment fragment = new BaseFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        if(getUserVisibleHint()){
+            isVisible = true;
+            onShowVisible();
+        }else{
+            isVisible = false;
+            onHideInvisible();
+        }
+    }
+
+    protected void onShowVisible(){
+        //TODO
+    }
+
+    protected void onHideInvisible(){
+        //TODO
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_base, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
